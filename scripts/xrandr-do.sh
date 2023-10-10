57ddf116870cb140adeb1f6dad5c1afc3a5fc905
paul <plapey45@gmail.com>
2023-10-10 00:56:09 -0400

    Update xrandr-do to be more robust

--- scripts/xrandr-do.sh
+++ scripts/xrandr-do.sh
@@ -2,20 +2,50 @@
 
 status=$( xrandr -q )
 
-# allows user to list monitors by priority
-declare -a monitors=(
+# Our custom ordering, optional
+declare -a monitor_order=(
+"DP1"
+"DP2"
 "DP-1"
 "DP-2"
 "DP-3"
+"DP2-1"
+"DP2-2"
+"DP2-3"
+"HDMI1"
+"HDMI2"
 "HDMI-1"
+"eDP1"
 "LVDS-1"
 )
 
+readarray -t outputs <<< "$(xrandr -q | grep '^[^ ]\+\s*\(dis\)\?connected' | awk '{print $1}')"
+echo "${outputs[@]}"
+
+# Add elements from monitor_order that exist in outputs to sorted array
+for mon in "${monitor_order[@]}"; do
+    for out in "${outputs[@]}"; do
+        if [[ "$mon" == "$out" ]]; then
+            sorted+=("$out")
+        fi
+    done
+done
+
+# Add elements from outputs that aren't in monitor_order to sorted array
+for out in "${outputs[@]}"; do
+    if ! printf '%s\n' "${monitor_order[@]}" | grep -q -P "^$out$"; then
+        sorted+=("$out")
+    fi
+done
+
+# Print the sorted array
+echo "${sorted[@]}"
+
 xrargs=""
 xcoord="0"
-for mon in "${monitors[@]}"
+for mon in "${sorted[@]}"
 do
-    if monstat=$( grep "$mon connected" <<< $status -A 1 ); then
+    if monstat=$( grep "^$mon connected" <<< $status -A 1 ); then
 	mode=$( tail -n 1 <<< $monstat | awk '{print $1}')
 	num_mons=$((num_mons+1))
 	monitor_width=${mode%x*} # from mode {xpos}x{ypos} truncate x{ypos}
