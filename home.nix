4e1240c41028082c623e02577f69fe879103b486
Paul Lapey <plapey45@gmail.com>
2024-07-26 10:54:40 -0400

    update home nix

--- home.nix
+++ home.nix
@@ -1,20 +1,26 @@
-{ config, pkgs, firefox-addons, custom-dwmblocks, nixvim, unstablePkgs, ... }:
-{
+{ config, pkgs, custom-dwmblocks, custom-dmenu, nixvim, unstablePkgs, ... }:
+let
+  mdcodecat =
+    pkgs.writeScriptBin "mdcodecat" (builtins.readFile ./mdcodecat.py);
+in {
   imports = [
     # For home-manager
     nixvim.homeManagerModules.nixvim
   ];
-  home.username = "alice";
-  home.homeDirectory = "/home/alice";
+  home.username = "paul";
+  home.homeDirectory = "/home/paul";
   nixpkgs.overlays = [
     (self: super: {
       dwmblocks =
         super.dwmblocks.overrideAttrs (oldattrs: { src = custom-dwmblocks; });
     })
+    (self: super: {
+      dmenu = super.dmenu.overrideAttrs (oldattrs: { src = custom-dmenu; });
+    })
   ];
   nixpkgs.config = {
     allowUnfree = true;
-    permittedInsecurePackages = [ "openssl-1.1.1w" ];
+    permittedInsecurePackages = [ "openssl-1.1.1w" "nix-2.16.2" ];
   };
 
   # This value determines the Home Manager release that your configuration is
@@ -26,37 +32,53 @@
   # release notes.
   home.stateVersion = "23.11"; # Please read the comment before changing.
 
-  home.packages = import ./packages.nix { inherit pkgs unstablePkgs; };
-
+  home.packages =
+    import ./packages.nix { inherit pkgs unstablePkgs mdcodecat; };
 
   programs.neovim = {
     enable = false;
     withPython3 = true;
-    plugins = with pkgs.vimPlugins; [ coq_nvim nvim-base16 ];
+    plugins = with pkgs.vimPlugins; [ coq_nvim ];
   };
 
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
     ".config/emacs/config.org".source = ./emacs/config.org;
     ".config/emacs/init.el".source = ./emacs/init.el;
     ".config/emacs/early-init.el".source = ./emacs/early-init.el;
-    ".config/emacs/setup_scripts".source = ./emacs/setup_scripts;
+    ".config/emacs/setup_scripts/buffer-move.el".source =
+      ./emacs/setup_scripts/buffer-move.el;
+    ".config/emacs/setup_scripts/elpaca-setup.el".source =
+      ./emacs/setup_scripts/elpaca-setup.el;
 
     # Uncomment if you want to manage neovim with config files
     # ".config/nvim/init.vim".source = ./nvim/init.vim;
     # ".config/nvim/coq-config.vim".source = ./nvim/coq-config.vim;
-    # ".config/nvim/lsp.lua".source = ./nvim/lsp.lua;
     ".config/sxhkd/sxhkdrc".source = ./sxhkd/sxhkdrc;
     ".config/aliasrc".source = ./aliasrc;
+    ".config/grab.sh".source = ./grab.sh;
+    ".config/unroll.sh".source = ./unroll.sh;
     ".zshrc".source = ./.zshrc;
     ".xinitrc".source = ./.xinitrc;
+    ".ssh/config".source = ./ssh/config;
 
   };
 
-  programs.firefox = { enable = true; };
+  programs.git = {
+    enable = true;
+    userName = "Paul Lapey";
+    userEmail = "plapey45@gmail.com";
+  };
   programs.home-manager.enable = true;
 
+  services.redshift = {
+    enable = true;
+    dawnTime = "08:00";
+    duskTime = "23:00";
+    temperature = {
+      day = 5500;
+      night = 3700;
+    };
+    provider = "geoclue2";
+  };
   programs.nixvim = import ./nixvim-config.nix { inherit pkgs; };
 }
