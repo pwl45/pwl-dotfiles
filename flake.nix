985c7a28462e709bff9467b4c317d23972a02680
Paul Lapey <plapey45@gmail.com>
2024-07-26 10:54:25 -0400

    remove firefox addons, they don't work

--- flake.nix
+++ flake.nix
@@ -9,10 +9,6 @@
       url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    firefox-addons = {
-      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     nixvim = {
       url = "github:nix-community/nixvim/nixos-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,34 +19,30 @@
     # };
     custom-dwmblocks.url = "github:pwl45/pwl-dwmblocks";
     custom-dwmblocks.flake = false;
+    custom-dmenu.url = "github:pwl45/dmenu-flexipatch";
+    custom-dmenu.flake = false;
   };
 
-  outputs = { nixpkgs, home-manager, firefox-addons, custom-dwmblocks, nixvim
+  outputs = { nixpkgs, home-manager, custom-dwmblocks, custom-dmenu, nixvim
     , nixpkgs-unstable, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      # pkgs.firefox-addons = firefox-addons;
     in {
-      homeConfigurations."alice" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."paul" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         extraSpecialArgs = {
           inherit custom-dwmblocks;
-          inherit firefox-addons;
+          inherit custom-dmenu;
           inherit nixvim;
           inherit system;
           unstablePkgs = nixpkgs-unstable.legacyPackages.${system};
         };
-        # specialArgs = {
-        #   # inherit firefox-addons;
-        # };       
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
         modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
       };
     };
 }
