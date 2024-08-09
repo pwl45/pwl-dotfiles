010ec52e55288fb8311f96695051b068615fa3ca
Paul Lapey <plapey45@gmail.com>
2024-08-08 20:18:29 -0400

    update wifi connect

--- scripts/network/wifi-connect.sh
+++ scripts/network/wifi-connect.sh
@@ -1,16 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 echo -n "retrieving wifi networks..." > $HOME/.status-msg # updating status
 
-# list networks, skip first line (which is always SSID), exclude lines with --, sort, pipe into dmenu,
-# store selected result in variable $netname
-netname=$( nmcli -f SSID device wifi list | tail -n +2 | grep -v "^--" | sort | uniq | dmenu | xargs)
+# list networks with SSID and security type, skip first line, exclude lines with --, sort, pipe into dmenu,
+# store selected result in variable $network_info
+network_info=$(nmcli -t -f SSID,SECURITY device wifi list | tail -n +2 | grep -v "^--" | sort | uniq | sed 's/:.\+/: [X]/g ; s/:$/: [O]/g' | dmenu)
+ 
+# Extract SSID and security type
+netname=$(echo "$network_info" | cut -d ':' -f 1)
+security=$(echo "$network_info" | cut -d ':' -f 2)
+
+echo $netname
+echo $security
+
 
 echo -n "connecting to $netname..." > $HOME/.status-msg # updating status
 
-# annoying: nmcli doesn't set $? to 1 on failed connection
-# so we have to grep for 'success' to determine if connection was successful
-# if it fails, dmenu for password connecct using that password
 nmcli dev wifi connect "$netname" 2>&1 | grep -qi "secrets.*required" &&
     dmenu -p "password for $netname?" | xargs nmcli dev wifi connect "$netname" password 
 
