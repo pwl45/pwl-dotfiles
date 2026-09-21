747882ced40fdb17e7c0ff9b6232fa50505a1143
Paul Lapey <plapey45@gmail.com>
2026-09-20 17:44:58 -0700

    xargs -r dssh

--- scripts/dssh.sh
+++ scripts/dssh.sh
@@ -11,4 +11,4 @@ if [[ $1 == '-t' ]]; then
   SSH_CMD="st -e ssh"
 fi
 
-grep '^Host' ${ssh_files[@]} -h| grep -v '\*' | awk '{print $2}' | dmenu | xargs $SSH_CMD
+grep '^Host' ${ssh_files[@]} -h| grep -v '\*' | awk '{print $2}' | dmenu | xargs -r $SSH_CMD
